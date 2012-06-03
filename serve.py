8db4cdcc85d04ae984a7b6828b536bd99da4f5ec
Jason R. Coombs <jaraco@jaraco.com>
2012-06-02 23:25:49 -0400

    Adding serve.py

---
+++ serve.py
@@ -0,0 +1,8 @@
+from roundup.cgi.wsgi_handler import RequestDispatcher
+import wsgiref.simple_server
+
+tracker_home = './Adams Row Tracker'
+app = RequestDispatcher(tracker_home)
+httpd = wsgiref.simple_server.make_server('', 8000, app)
+print "Listening on port 8000...."
+httpd.serve_forever()
