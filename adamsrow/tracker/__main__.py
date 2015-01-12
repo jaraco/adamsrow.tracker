d2d73f4d284110686f3fdfefe4c11631bfe44f02
Jason R. Coombs <jaraco@jaraco.com>
2015-01-11 23:00:13 -0500

    Add compatibility with Python 3

--- adamsrow/tracker/__main__.py
+++ adamsrow/tracker/__main__.py
@@ -4,5 +4,5 @@ import wsgiref.simple_server
 tracker_home = './Adams Row Tracker'
 app = RequestDispatcher(tracker_home)
 httpd = wsgiref.simple_server.make_server('', 8000, app)
-print "Listening on port 8000...."
+print("Listening on port 8000....")
 httpd.serve_forever()
