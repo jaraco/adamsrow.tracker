4fc0fbc0f69f1c5f53eb854d24173afbe457eaa6
Jason R. Coombs <jaraco@jaraco.com>
2015-01-17 09:09:55 -0500

    Options must be optional

--- adamsrow/tracker/__main__.py
+++ adamsrow/tracker/__main__.py
@@ -7,7 +7,7 @@ from roundup.cgi.wsgi_handler import RequestDispatcher
 
 def get_args():
 	parser = argparse.ArgumentParser()
-	parser.add_argument('site_home', default='site')
+	parser.add_argument('--site', default='site')
 	default_port = int(os.environ.get('PORT', 8000))
 	parser.add_argument('--port', type=int, default=default_port)
 	return parser.parse_args()
@@ -15,7 +15,7 @@ def get_args():
 def run():
 	args = get_args()
 
-	app = RequestDispatcher(args.site_home)
+	app = RequestDispatcher(args.site)
 	httpd = wsgiref.simple_server.make_server('', args.port, app)
 	print("Listening on port {port}...".format(**locals()))
 	httpd.serve_forever()
